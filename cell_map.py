95fd80807ab2eea35fbe4651ac4adf7c8c56945b
1g0r2005 <64415553+1g0r2005@users.noreply.github.com>
2023-10-05 10:20:17 +0300

    updated walls and pallete, add demo player

--- cell_map.py
+++ cell_map.py
@@ -9,7 +9,7 @@ alive = [0,1,2,3]
 dead = [5,6,7,8]
 
 cellsize = WIDTH//N,HEIGHT//N
-
+print(cellsize)
 pygame.init()
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 clock = pygame.time.Clock()
@@ -56,22 +56,6 @@ def init_gen(N,min_shift,max_shift,min_size,max_size):
                 else:
                     cells[x][y]=1
     return cells
-
-def draw_cell(screen,matrix,color_matrix=screen):
-    '''
-    draw new state of cells by matrix
-    screen - pygame screen object
-    matrix - bitmap of cells states
-    color - map of cells colors
-    '''
-    for x in range(N):
-        for y in range(N):
-            if matrix[x][y] == 1:
-                single_color = 255*(1-color_matrix[x][y]/8)
-                color = (single_color,single_color,single_color)
-            else:
-                color = (255*(7/8),255*(7/8),255*(7/8))
-            pygame.draw.rect(screen,color,[x*cellsize[0],y*cellsize[1],(x+1)*cellsize[0],(y+1)*cellsize[1]])
 def neibourhood(matrix):
     '''
     return matrix with count of alive neibours of cell
@@ -113,9 +97,7 @@ def generate(cell_matrix,nb_matrix,alive,dead):
                     cell_matrix[x][y] = 1
             else:
                 if neibour in alive: #1 to 0 (wall to air)
-                    cell_matrix[x][y] = 0
-
-                 
+                    cell_matrix[x][y] = 0               
 def post_gen(cell_matrix,color_matrix):
     '''
     fix generation artefacts
@@ -158,21 +140,102 @@ def post_gen(cell_matrix,color_matrix):
                     else:
                         color_matrix[x][y] = color_matrix[x][y-1]+1
                         
-                             
+def draw_cell(screen,matrix,color_matrix=screen):
+    '''
+    draw new state of cells by matrix
+    screen - pygame screen object
+    matrix - bitmap of cells states
+    color - map of cells colors
+    '''
+    for x in range(N):
+        for y in range(N):
+            if matrix[x][y] == 1:
+                color = list(map(lambda single_color: single_color*(1-color_matrix[x][y]/9),[255,191,128]))#255*(1-color_matrix[x][y]/8)
+            else:
+                color = (255*(7/8),204*(7/8),153*(7/8))
+            pygame.draw.rect(screen,color,[x*cellsize[0],y*cellsize[1],(x+1)*cellsize[0],(y+1)*cellsize[1]])     
+
+def draw_player(screen,x,y):
+    '''
+    draw position of player
+    screen - pygame screen object
+    x,y - coords of player cell
+    '''
+    PLAYER_COLOR = (255,0,0)
+   
+    pygame.draw.rect(screen,PLAYER_COLOR,[x*cellsize[0],y*cellsize[1],cellsize[0],cellsize[1]])
+def move_player(x0,y0,dir,cellmap):
+    '''
+    return new position of player
+    use keyboard events
+    x0,y0 - coords of player
+    dir - direction of movement
+    [left = [-1,0]]
+    [right = [1,0]]
+    [up = [0,-1]]
+    [down = [0,1]]
+    cellmap - bitmap of cells states (1=wall,0=air)
+    '''
+    x1,y1 = x0,y0
+    if dir[0]==1: #down
+        if y0!=N-1:
+            if cellmap[x0][y0+1]==0:
+                y1+=1
+    elif dir[0]==-1: #up
+        if y0!=0:
+            if cellmap[x0][y0-1]==0:
+                y1-=1
+    if dir[1]==1: #right
+        if x0!=N-1:
+            if cellmap[x0+1][y0]==0:
+                x1+=1
+    elif dir[1]==-1: #left
+        if x0!=0:
+            if cellmap[x0-1][y0]==0:
+                x1-=1
+    return x1,y1
+
+'''map generation'''
 cells = init_gen(N,5,10,0,8)
-##gen map
 maxtick = 4
 for i in range(maxtick):
     generate(cells,neibourhood(cells),alive,dead)
 color_cells = neibourhood(cells)
 post_gen(cells,color_cells)
+'''initial player pos [temp]'''
+
+flag = True
+while flag:
+    x,y = random.randint(0,N-1),random.randint(0,N-1)
+    if cells[x][y]==0:
+        flag = False
+        print(x,y)
 
 running = True
+motion = [0,0]
+
+
 while running:
     clock.tick(FPS)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-    
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT: #left
+                motion = [0,-1]
+            elif event.key == pygame.K_RIGHT: #right
+                motion = [0,1]
+            elif event.key == pygame.K_UP: #up
+                motion = [-1,0]
+            elif event.key == pygame.K_DOWN: #down
+                motion = [1,0]
+        elif event.type == pygame.KEYUP:
+            if event.key in [pygame.K_LEFT,
+                         pygame.K_RIGHT,pygame.K_UP,pygame.K_DOWN]:
+                motion = [0,0]
     draw_cell(screen,cells,color_matrix=color_cells)
+    x,y = move_player(x,y,motion,cells)
+    draw_player(screen,x,y)
+
+    
     pygame.display.flip()
